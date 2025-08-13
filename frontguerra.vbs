0163ae244d52ac84120603ee7debcb0917df4a54
unknown <tonyusb@gmail.com>
2025-08-13 09:52:50 -0500

    First commit

---
+++ frontguerra.vbs
@@ -0,0 +1,2 @@
+Set objShell = WScript.CreateObject("WScript.Shell")
+objShell.Run "C:\SERVER\GUERRA\front\ecenderGuerrafront.bat", 0, True
---
+++ src/app/componentes/home.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+    return (
+        <div className="App">
+            GUERRA PROJECT MANAGER
+        </div>
+    );
+}
+
+export default App;
